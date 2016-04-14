a32917e8185b958acd64f4f9c2edbd505df09fde
Carlos A <darkklitos@gmail.com>
2016-04-14 00:05:48 -0500

    Nuevo diseño de la API para poder modificar datos en la db

---
+++ ApiSilabo/src/java/org/api/data/Docente.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.api.data;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author DarkKlitos
+ */
+@Entity
+@Table(name = "docente")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Docente.findAll", query = "SELECT d FROM Docente d"),
+    @NamedQuery(name = "Docente.findById", query = "SELECT d FROM Docente d WHERE d.id = :id"),
+    @NamedQuery(name = "Docente.findByCedula", query = "SELECT d FROM Docente d WHERE d.cedula = :cedula"),
+    @NamedQuery(name = "Docente.findByNombre", query = "SELECT d FROM Docente d WHERE d.nombre = :nombre"),
+    @NamedQuery(name = "Docente.findByCategoria", query = "SELECT d FROM Docente d WHERE d.categoria = :categoria")})
+public class Docente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Size(max = 20)
+    @Column(name = "cedula")
+    private String cedula;
+    @Size(max = 50)
+    @Column(name = "nombre")
+    private String nombre;
+    @Size(max = 15)
+    @Column(name = "categoria")
+    private String categoria;
+
+    public Docente() {
+    }
+
+    public Docente(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Docente)) {
+            return false;
+        }
+        Docente other = (Docente) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "org.api.data.Docente[ id=" + id + " ]";
+    }
+    
+}
---
+++ ApiSilabo/src/java/org/api/data/service/DocenteFacadeREST.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.api.data.service;
+import org.api.data.Docente;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author DarkKlitos
+ */
+@Stateless
+@Path("/docente")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class DocenteFacadeREST extends AbstractFacade<Docente> {
+    @PersistenceContext(unitName = "ApiSilaboPU") 
+    private EntityManager em;
+
+    public DocenteFacadeREST() {
+        super(Docente.class);
+    }
+
+    @POST
+    @Override
+    public void create(Docente entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    public void edit(@PathParam("id") int id, Docente entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") int id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    public Docente find(@PathParam("id") int id) {
+        return super.find(id);
+    }
+
+    @GET
+    @Override
+    public List<Docente> findAll() {
+        return super.findAll();
+    }
+
+    /**
+     * Find a list of teachers given certain parameters.
+     *
+     * <code>
+     *
+     *      /profesor/buscar?first_name=John&last_name=Doe&document_number=576944&gender=masculino
+     *
+     * </code>
+     *
+     * @return Procesor list
+     */
+    @GET
+    @Path("buscar")
+    public List<Docente> search(
+        @DefaultValue("") @QueryParam("cedula") String cedula,
+        @DefaultValue("") @QueryParam("nombre") String nombre,
+        @DefaultValue("") @QueryParam("categoria") String categoria
+//        @DefaultValue("") @QueryParam("id") int id
+    ) {
+        // Returns a list of teacher filterd by GET parameters.
+        return em.createNamedQuery("Docente.findByParams")
+            .setParameter("cedula", "%" + cedula + "%")
+            .setParameter("nombre", "%" + nombre + "%")
+            .setParameter("categoria", "%" + categoria + "%")
+            // .setParameter("id", "%" + id + "%") 
+            .getResultList();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    public List<Docente> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces("text/plain")
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+}
