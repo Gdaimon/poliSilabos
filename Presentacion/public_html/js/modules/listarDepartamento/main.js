8f293fe6a190d97030da084a0b49ca04c47e7866
Mauricio Nishida <mauri_n@yahoo.com>
2016-05-16 15:37:11 -0500

    Add files via upload

--- Presentacion/public_html/js/modules/listarDepartamento/main.js
+++ Presentacion/public_html/js/modules/listarDepartamento/main.js
@@ -21,7 +21,7 @@ require.config({
 require([
     // Load our app module and pass it to our definition function
     // 'modules/docente/app'
-    'modules/docente/app'
+    'modules/departamento/app'
 
 ], function (App) {
     // The "app" dependency is passed in as "App"
