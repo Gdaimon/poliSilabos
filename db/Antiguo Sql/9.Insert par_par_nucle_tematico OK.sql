da7cd3dd71be55ad678baa08833403ea03db01f5
dagaitanb <dalejandro.gaitan@gmail.com>
2016-05-17 00:25:43 -0500

    Update 9.Insert par_par_nucle_tematico OK.sql

--- db/Antiguo Sql/9.Insert par_par_nucle_tematico OK.sql
+++ db/Antiguo Sql/9.Insert par_par_nucle_tematico OK.sql
@@ -1,1 +1,16 @@
-﻿INSERT INTO par_nucleo_tematico(	"id_materia",	"descripcion",	"fecha_creacion",	"estado_nucleo")VALUES	(		1002,		'Conceptos básicos y diseño de producto',		current_timestamp,		true	),	(		1002,		'Conceptos básicos y diseño de producto',		current_timestamp,		true	),	(		1002,		'Gestion de proyectos.',		current_timestamp,		true	),	(		1002,		'Estudio del trabajo.',		current_timestamp,		true	),	(		1002,		'Medición de la capacidad',		current_timestamp,		true	),	(		1002,		'Distribución y ubicación de planta.',		current_timestamp,		true	),	(		1002,		'Estudio de materiales.',		current_timestamp,		true	),	(		1002,		'Conceptos básicos y diseño de producto',		current_timestamp,		true	),	(		1002,		'Conceptos básicos y diseño de producto',		current_timestamp,		true	),	(		1002,		'Gestion de proyectos.',		current_timestamp,		true	),	(		1002,		'Estudio del trabajo.',		current_timestamp,		true	),	(		1002,		'Medición de la capacidad',		current_timestamp,		true	),	(		1002,		'Distribución y ubicación de planta.',		current_timestamp,		true	),	(		1002,		'Estudio de materiales.',		current_timestamp,		true	);
+insert into par_nucleo_tematico("cod_materia","nombre","fecha_creacion","estado_nucleo")
+values
+(1002,'Conceptos básicos y diseño de producto',current_timestamp,TRUE),
+(1002,'Conceptos básicos y diseño de producto',current_timestamp,TRUE),
+(1002,'Gestion de proyectos.',current_timestamp,TRUE),
+(1002,'Estudio del trabajo.',current_timestamp,TRUE),
+(1002,'Medición de la capacidad',current_timestamp,TRUE),
+(1002,'Distribución y ubicación de planta.',current_timestamp,TRUE),
+(1002,'Estudio de materiales.',current_timestamp,TRUE),
+(1002,'Conceptos básicos y diseño de producto',current_timestamp,TRUE),
+(1002,'Conceptos básicos y diseño de producto',current_timestamp,TRUE),
+(1002,'Gestion de proyectos.',current_timestamp,TRUE),
+(1002,'Estudio del trabajo.',current_timestamp,TRUE),
+(1002,'Medición de la capacidad',current_timestamp,TRUE),
+(1002,'Distribución y ubicación de planta.',current_timestamp,TRUE),
+(1002,'Estudio de materiales.',current_timestamp,TRUE);
