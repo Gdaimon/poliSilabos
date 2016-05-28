7974db7259cda8b9813758a0bcd5060c82091039
dagaitanb <dalejandro.gaitan@gmail.com>
2016-05-28 10:19:09 -0500

    Correccion Import

--- ApiSilabo/src/java/org/api/data/service/SilaboFacadeREST.java
+++ ApiSilabo/src/java/org/api/data/service/SilaboFacadeREST.java
@@ -2,7 +2,6 @@ package org.api.data.service;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.Instant;
 import java.util.Date;
 import java.util.List;
 import javax.ejb.Stateless;
